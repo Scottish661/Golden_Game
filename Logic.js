5256a3723a09e45db8f0a8173c691345348c8d14
Scottish661 <h0785014@gmail.com>
2026-09-25 09:04:04 +0100

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -1,5 +1,20 @@
 let canvas = document.getElementById('canvas');
 let pen = canvas.getContext('2d');
 const CS = 512;
-canvas.height = CS;
 canvas.width = CS;
+canvas.height = CS;
+class Player {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    draw() {
+        pen.fillStyle = 'red'; 
+        pen.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+const player = new Player(50, 50, 30, 40);
+pen.clearRect(0, 0, CS, CS);
+player.draw();
