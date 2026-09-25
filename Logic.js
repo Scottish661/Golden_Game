19e1d4a695af561c2dddd62fdd69e9bc5bd74412
Scottish661 <h0785014@gmail.com>
2026-09-25 18:28:49 +0100

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -16,7 +16,7 @@
             pen.fillRect(this.x, this.y, this.width, this.height);
         }
         gravity(){
-            this.y (this.y + 2) % CS;
+            this.y  = (this.y + 2) % CS;
             }
     }
     const player = new Player(50, 50, 30, 40,'blue')
